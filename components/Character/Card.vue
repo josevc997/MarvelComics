f2c3b045b5f5da2faccc47c4596e7e5770561055
Jose Valencia <jose.valencia@ctsturismo.cl>
2024-01-26 20:52:31 -0300

    chore: update nuxt version

--- components/Character/Card.vue
+++ components/Character/Card.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
-import { Character } from "~/types/characters";
+import type { Character } from "~/types/characters";
 defineProps<{
-  character: Character;
+    character: any;
 }>();
 </script>
 <template>
-  <NuxtLink
-    class="grid col-span-4 sm:col-span-2 md:col-span-1 place-content-between border rounded-md border-slate-300 p-2 gap-2 hover:shadow-md transition hover:scale-105 duration-300 ease-in-out cursor-pointer"
-    :to="{ name: 'character-id', params: { id: character.id } }"
-  >
-    <h2 class="text-base font-medium">{{ character.name }}</h2>
-    <NuxtImg
-      :src="character.thumbnail.path + '.' + character.thumbnail.extension"
-      :alt="character.name"
-      format="webp"
-      class="w-full aspect-[1] object-fill rounded-md"
-      loading="lazy"
-    />
-    <!-- sizes="xs:100vw sm:50vw md:223px" -->
-  </NuxtLink>
+    <NuxtLink
+        class="grid col-span-4 sm:col-span-2 md:col-span-1 place-content-between border rounded-md border-slate-300 p-2 gap-2 hover:shadow-md transition hover:scale-105 duration-300 ease-in-out cursor-pointer"
+        :to="{ name: 'character-id', params: { id: character.id } }"
+    >
+        <h2 class="text-base font-medium">{{ character.name }}</h2>
+        <NuxtImg
+            :src="
+                character.thumbnail.path + '.' + character.thumbnail.extension
+            "
+            :alt="character.name"
+            format="webp"
+            class="w-full aspect-[1] object-fill rounded-md"
+            loading="lazy"
+        />
+        <!-- sizes="xs:100vw sm:50vw md:223px" -->
+    </NuxtLink>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import { error } from "console";
 import CharacterService from "~/services/Character.service";
-import { characterListResponse } from "~/types/characters";
-import { CustomResponse } from "~/types/response";
+import type { characterListResponse } from "~/types/characters";
+import type { CustomResponse } from "~/types/response";
 
 const route = useRoute();
 const characterStore = useCharacterStore();
@@ -11,60 +10,64 @@ const data = ref({} as CustomResponse<characterListResponse>);
 const message = ref(null as string | null);
 
 const offset = computed(() => {
-  return route.query.offset;
+    return route.query.offset;
 });
 
 const handleSearch = async () => {
-  characterStore.fetchCharacterList(Number(route.query.offset));
+    characterStore.fetchCharacterList(Number(route.query.offset));
 };
 
 handleSearch();
 
 watch(offset, async () => {
-  handleSearch();
+    handleSearch();
 });
 useHead({
-  title: "Character List",
-  meta: [
-    {
-      hid: "description",
-      name: "description",
-      content: "Characters from Marvel API",
+    title: "Character List",
+    meta: [
+        {
+            hid: "description",
+            name: "description",
+            content: "Characters from Marvel API",
+        },
+    ],
+    htmlAttrs: {
+        lang: "en",
     },
-  ],
-  htmlAttrs: {
-    lang: "en",
-  },
 });
 </script>
 <template>
-  <div>
-    <template v-if="message !== null">
-      <h1>{{ message }}</h1>
-    </template>
-    <template v-else>
-      <h1 class="text-3xl font-medium mb-4">Characters</h1>
-      <ClientOnly>
-        <div
-          v-if="characterStore.characterList.length > 0"
-          class="grid grid-cols-4 gap-4"
-        >
-          <CharacterCard
-            v-for="character in characterStore.characterList"
-            :key="character.id"
-            :character="character"
-          />
-        </div>
-        <div>
-          {{ characterStore.offset + characterStore.limit }}
-          <NuxtLink
-            :to="{
-              query: { offset: characterStore.offset + characterStore.limit },
-            }"
-            >See more</NuxtLink
-          >
-        </div>
-      </ClientOnly>
-    </template>
-  </div>
+    <div>
+        <template v-if="message !== null">
+            <h1>{{ message }}</h1>
+        </template>
+        <template v-else>
+            <h1 class="text-3xl font-medium mb-4">Characters</h1>
+            <ClientOnly>
+                <div
+                    v-if="characterStore.characterList.length > 0"
+                    class="grid grid-cols-4 gap-4"
+                >
+                    <CharacterCard
+                        v-for="character in characterStore.characterList"
+                        :key="character.id"
+                        :character="character"
+                    />
+                </div>
+                <div>
+                    {{ characterStore.offset + characterStore.limit }}
+                    <NuxtLink
+                        :to="{
+                            query: {
+                                offset:
+                                    characterStore.offset +
+                                    characterStore.limit,
+                            },
+                        }"
+                        >See more</NuxtLink
+                    >
+                </div>
+            </ClientOnly>
+        </template>
+    </div>
 </template>
