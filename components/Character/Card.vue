ae20cf0de4b8c571667026ddb6aafd6f12bc695d
Jose Valencia <jose.valencia@ctsturismo.cl>
2024-01-27 11:14:29 -0300

    feat: update index styles

--- components/Character/Card.vue
+++ components/Character/Card.vue
@@ -6,19 +6,22 @@ defineProps<{
 </script>
 <template>
     <NuxtLink
-        class="grid col-span-4 xs:col-span-2 md:col-span-1 place-content-between border rounded-md border-slate-300 p-2 gap-2 hover:shadow-md transition hover:scale-105 duration-300 ease-in-out cursor-pointer"
+        class="grid grid-cols-1 col-span-4 xs:col-span-2 md:col-span-1 place-content-between border rounded-md border-slate-300 p-2 gap-2 hover:shadow-md transition group duration-300 ease-in-out cursor-pointer"
         :to="{ name: 'character-id', params: { id: character.id } }"
     >
-        <h2 class="text-base font-medium">{{ character.name }}</h2>
-        <NuxtImg
-            :src="
-                character.thumbnail.path + '.' + character.thumbnail.extension
-            "
-            :alt="character.name"
-            format="webp"
-            class="w-full aspect-[1] object-fill rounded-md"
-            loading="lazy"
-        />
-        <!-- sizes="xs:100vw sm:50vw md:223px" -->
+        <div class="overflow-hidden rounded-md w-full col-span-1 border">
+            <NuxtImg
+                :src="
+                    character.thumbnail.path +
+                    '.' +
+                    character.thumbnail.extension
+                "
+                :alt="character.name"
+                format="webp"
+                class="w-full aspect-[1] object-fill rounded-md group-hover:scale-105 transition duration-300 ease-in-out"
+                loading="lazy"
+            />
+        </div>
+        <h2 class="text-lg font-medium">{{ character.name }}</h2>
     </NuxtLink>
 </template>
---
+++ components/UI/PaginationItem.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+const NuxtLink = resolveComponent("nuxt-link");
+const props = defineProps<{
+    index?: number;
+    first?: boolean;
+    last?: boolean;
+}>();
+
+const characterStore = useCharacterStore();
+
+const disabled = computed(() => {
+    if (props.first) {
+        return characterStore.offset - characterStore.limit < 0;
+    }
+    if (props.last) {
+        return (
+            characterStore.total < characterStore.offset + characterStore.limit
+        );
+    }
+    return false;
+});
+
+const offset = computed(() => {
+    if (props.index) {
+        return characterStore.offset + characterStore.limit * props.index;
+    }
+    if (props.last) {
+        return (
+            Math.trunc(characterStore.total / characterStore.limit) *
+            characterStore.limit
+        );
+    }
+});
+
+const showItem = computed(() => {
+    if (props.first || props.last) {
+        return true;
+    }
+    if (props.index) {
+        if (props.index > 0) {
+            return (
+                characterStore.total >
+                characterStore.offset + characterStore.limit * props.index
+            );
+        } else {
+            return (
+                0 <= characterStore.offset + characterStore.limit * props.index
+            );
+        }
+    }
+});
+const pageNumber = computed(() => {
+    if (props.index) {
+        return characterStore.offset / characterStore.limit + props.index + 1;
+    }
+});
+</script>
+<template>
+    <component
+        :is="disabled ? 'div' : NuxtLink"
+        :to="{
+            query: {
+                offset: offset,
+            },
+        }"
+        v-if="showItem"
+        :disabled="disabled"
+    >
+        <button
+            :disabled="disabled"
+            class="border flex items-center justify-center hover:ring-1 hover:ring-green-600/60 border-slate-400 px-1 py-1 aspect-square rounded size-9 hover:bg-slate-200 disabled:ring-0 disabled:bg-transparent text-sm font-medium"
+        >
+            <template v-if="first || last">
+                <Icon
+                    :name="
+                        first
+                            ? 'heroicons:chevron-double-left-20-solid'
+                            : 'heroicons:chevron-double-right-20-solid'
+                    "
+                    class="size-6"
+                />
+            </template>
+            <template v-else>
+                {{ pageNumber }}
+            </template>
+        </button>
+    </component>
+</template>
